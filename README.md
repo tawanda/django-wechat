ebe659eb565116115444bd67c51eac77104f70bd
tawanda <tminya@gmail.com>
2015-11-26 09:57:48 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
 # django-wechat
 Access WeChat functions using django views
+
+tested in python 3.5 and django 1.8, but should work with lower versions of both
+
+This is more a code snippet rather than an actual app
+
+Currently this snippet is only for reading WeChat messages, but you can extend it, pull requests welcome
+
+
+## useful videos
+
+these videos give you an overview of how to setup WeChat. They are in PHP but the message is clear
+
+[video 1](https://www.youtube.com/watch?v=kB20Zf51QWU)
+
+[video 2](https://www.youtube.com/watch?v=_2FSzD2B2F0)
+
+
+# 1) Project setup
+
+To begin create your app and make your custom url point to our
+wechat view
+
+`http://mysite/wechat/`
+
+subclass the view in the wechat directory of this project
+
+```python
+
+    from wechat.views import WeChatView
+
+
+    MyCustomView(WeChatView):
+        token = "ad4sf65weG7Db6ddWE"
+        
+        on_message(self, message):
+                
+            root = ET.fromstring(message)
+
+            from = root[1].text
+            message_type = root[3].text
+            content = root[4].text
+
+            print('from: {}'.format(from))
+            print('message type: {}'.format(message_type))
+            print('content: {}'.format(content))
+        
+```
+
+
+#### token
+
+You need to set this yourself, use any alphanumeric string, if you use any special characters WeChat 
+wont authenticate
+
+
+#### on_message
+
+The messages come as xml, so here we use an xml reader just to print out the message. to see all
+the different message types check here
+
+[WeChat Message Types](http://admin.wechat.com/wiki/index.php?title=Common_Messages)
+
+
+# 2) Creating a WeChat Developer account
+
+### Create developer account
+
+* head over to [http://admin.wechat.com/debug/sandbox](http://admin.wechat.com/debug/sandbox)
+* Click on Login
+* Scan the QR code
+
+You should see the sandbox developer page now
+
+### Authenticate your app
+
+In the developer page look for the heading `API config` there will be entry fields for URL and Token
+
+* Ensure your web app is running and your custom view is accessible on http://mysite/wechat/
+* If you are testing on localhost a good tool to use is [ngrok](https://ngrok.com/docs#expose)
+
+URL: enter the url you chose for your view e.g. `http://mysite/wechat/` or the same url if you are using 
+ngrok to expose your localhost to the outside world could be e.g. `http://bfwr41f123b.ngrok.io/wechat/`
+
+TOKEN: enter the token you chose in step 1), remember it has to be the same as your views token and only alphanumeric
+
+once you click ok you should see a message that says successful or something similar
+
+### Add 
---
+++ wechat/views.py
@@ -0,0 +1,91 @@
+import hashlib
+import logging
+
+from django.http import HttpResponse
+from django.utils.datastructures import MultiValueDictKeyError
+from django.views.generic import View
+
+
+logger = logging.getLogger('wechat.api')
+
+
+# noinspection PyMethodMayBeStatic
+class WeChatView(View):
+    """
+    WeChat API
+    """
+    token = ""
+
+    def get(self, request, *args, **kwargs):
+        logger.info('- New GET From WeChat -')
+
+        authenticated, data = self.authenticate(request, kwargs)
+
+        if authenticated:
+            if data['echostr']:
+                    logger.info('sending response..')
+                    logger.info('echostr: {}'.format(data['echostr']))
+                    return HttpResponse(data['echostr'])
+            else:
+                logger.info('WeChat Authenticated, but nonce is missing in a GET request, signature:{} timestamp:{} '
+                            'nonce:{}'.format(data['signature'], data['timestamp'], data['nonce']))
+                return HttpResponse(status=200)
+
+        return HttpResponse(status=403)
+
+    def post(self, request, *args, **kwargs):
+        logger.info('- New POST From WeChat -')
+
+        authenticated, data = self.authenticate(request, kwargs)
+
+        if authenticated:
+            # This is what we send to the message processing task
+            logger.info('New message received: {}'.format(request.body))
+
+            self.on_message(message=request.body)
+            return HttpResponse(status=201)
+
+        return HttpResponse(status=403)
+
+    def authenticate(self, request, kwargs):
+        data = dict()
+
+        try:
+            signature = request.GET['signature']
+            timestamp = request.GET['timestamp']
+            nonce = request.GET['nonce']
+            echostr = request.GET.get('echostr', '')
+
+        except MultiValueDictKeyError:
+            logger.error('missing parameters in WeChat request')
+            return False, data
+
+        if not self.token:
+            logger.error('Token not set on class instance')
+            return False, data
+
+        params = [self.token, timestamp, nonce]
+        params.sort()
+        auth_string = ''.join(params)
+
+        if self.validate_signature(auth_string, signature):
+            data.update({
+               'signature': signature,
+               'timestamp': timestamp,
+               'nonce': nonce,
+               'echostr': echostr,
+               'token': self.token,
+            })
+            return True, data
+        else:
+            logger.info('WeChat Authentication failed, signature:{} timestamp:{} nonce:{}'
+                        .format(signature, timestamp, nonce))
+            return False, data
+
+    @staticmethod
+    def validate_signature(auth_string, signature):
+        return hashlib.sha1(auth_string.encode()).hexdigest() == signature
+
+    def on_message(self, message):
+        # do something with the message here
+        raise NotImplementedError('You need to do something with the message recieved')
